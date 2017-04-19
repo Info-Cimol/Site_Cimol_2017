87fecfce83e44cf979cd490f922cbfd67a421f4a
candidofarias <cdcfarias@gmail.com>
2017-04-19 20:32:52 -0300

    Atualização

--- application/modules/admin/views/temas/default/default.php
+++ application/modules/admin/views/temas/default/default.php
@@ -4,9 +4,3 @@ require_once("content.php");
 require_once("mensagens.php");
 require_once(APPPATH."modules/admin/views/".$template.".php");
 require_once("footer.php");
-/*
-$CI->load->view('templates/header', $vars);
-$CI->load->view($template, $vars);
-$CI->load->view('templates/content', $vars);
-$CI->load->view('templates/footer', $vars);
-*/
