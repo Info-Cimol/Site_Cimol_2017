db637f197c0aef018f8ff1aa4e3ebda21e8032c7
candidofarias <cdcfarias@gmail.com>
2017-05-08 15:09:05 -0300

    Atualização

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -1,26 +1,25 @@
 <?php
 class Usuario_model extends CI_Model{
 	function autenticar($usuario, $senha){
-		
-                $query="SELECT usuario.id, pessoa.nome, 
-						pessoa.rg, pessoa.cpf, 
-						count(administrador.pessoa_id) as admin,
-						count(aluno.pessoa_id) as aluno,
-						count(professor.pessoa_id) as professor,
-                		count(feintec.pessoa_id) as feintec,
-                		count(biblioteca.pessoa_id) as biblioteca,
-                		count(coordenador_curso.professor_id) as coordenador_curso,
-						count(pessoa.id) as pessoa from pessoa LEFT JOIN usuario ON usuario.pessoa_id = pessoa.id 
-                                                LEFT JOIN email ON email.pessoa_id=pessoa.id 
-                                                LEFT JOIN administrador ON administrador.pessoa_id=pessoa.id 
-                                                LEFT JOIN aluno ON aluno.pessoa_id=pessoa.id 
-                                                LEFT JOIN professor ON professor.pessoa_id=pessoa.id 
-                								LEFT JOIN feintec ON feintec.pessoa_id=pessoa.id 
-                								LEFT JOIN biblioteca ON biblioteca.pessoa_id=pessoa.id 
-                								LEFT JOIN coordenador_curso ON coordenador_curso.professor_id=professor.id
-                                                WHERE usuario.senha = '".$senha."' AND email.email = '".$usuario."'";
-                  $result = $this->db->query($query); 
-                  return $result;
+        $query="SELECT usuario.id, pessoa.nome,pessoa.rg, pessoa.cpf, 
+		count(administrador.pessoa_id) as admin,
+		count(aluno.pessoa_id) as aluno,
+		count(professor.pessoa_id) as professor,
+        count(feintec.pessoa_id) as feintec,
+        count(biblioteca.pessoa_id) as biblioteca,
+        count(coordenador_curso.professor_id) as coordenador_curso,
+		count(pessoa.id) as pessoa from pessoa 
+        	LEFT JOIN usuario ON usuario.pessoa_id = pessoa.id 
+            LEFT JOIN email ON email.pessoa_id=pessoa.id 
+            LEFT JOIN administrador ON administrador.pessoa_id=pessoa.id 
+            LEFT JOIN aluno ON aluno.pessoa_id=pessoa.id 
+            LEFT JOIN professor ON professor.pessoa_id=pessoa.id 
+            LEFT JOIN feintec ON feintec.pessoa_id=pessoa.id 
+            LEFT JOIN biblioteca ON biblioteca.pessoa_id=pessoa.id 
+            LEFT JOIN coordenador_curso ON coordenador_curso.professor_id=professor.id
+            WHERE usuario.senha = '".$senha."' AND email.email = '".$usuario."'";
+       $result = $this->db->query($query); 
+       return $result;
 				/*$query = $this->db->get();
 				return $query;*/
 	}
