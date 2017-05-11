c685671e6453b67377dd6d148981402de08556c3
anath <anath@DESKTOP-9H19KIO>
2017-05-11 20:19:15 -0300

    redefinir senha

---
+++ tema/template/red-senha/definir-senha/definir_senha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <title>Login</title>
+        <meta name="author" content="Ana Thais">
+        <link rel="stylesheet"  href="./css/style.css" media="screen" >
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" >
+    </head>
+    <body>
+        <section>
+            <div class="fechar">X</div>
+            <div id="imagem"><img src="images/cimol_branco.png"/></div>
+            <div id="sbv"><strong><p>DEFINA SUA SENHA</p></strong></div>
+            <div id="msg"><p>VOCÊ RECEBERÁ UMA MENSAGEM CONTENDO INSTRUÇÕES VISANDO SUA DEFINIÇÃO DE SENHA</p></div>
+            <div id="def"><p>Email de definição:</p></div>
+            <div id="carta-email">
+                <img src="images/email.png"/>
+                <!--<?php
+                    $var_email=$_POST['email'];
+                    echo "$var_email";
+                php?>-->
+            </div>
+        </section>
+    </body>
+</html>
