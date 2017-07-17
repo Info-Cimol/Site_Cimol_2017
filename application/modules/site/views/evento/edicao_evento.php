5797b6dbbfa43079940fe6af41f05d6237678b98
anath <anath@DESKTOP-9H19KIO>
2017-07-17 13:32:22 -0300

    Edição evento

---
+++ application/modules/site/views/evento/edicao_evento.php
@@ -0,0 +1,160 @@
+<link rel="stylesheet" href="<?php echo base_url();  ?>public/site/css/evento.css" />
+
+<div id="conteudo">
+            
+            <div id="imagem">               
+                <img src="images/feintec logo desktop.png"/>
+            </div>
+            
+            <div id="feintec">
+                <p><strong>9ª FEINTEC</strong></p>
+            </div>
+            
+            <div id="sobre-evento">
+                <p>O CHÃO É O LIMITE</p>
+                <p>De 27-05-2016 à 30-07-2016</p>
+            </div>
+            
+            <div id="imagem_evento">
+                <h3>IMAGENS</h3>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                    <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                <a href="images/feintec.png" data-lightbox="galeria" >               
+                
+                <img src="images/feintec.png" class="img-thurmbnail"/>
+                
+                </a>
+                
+                
+                
+                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Título do modal</h4>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary">Salvar mudanças</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+            </div>
+            
+            <div id="painel_evento">
+                
+                <h3>PAINEIS</h3>
+                
+                <div class="item-evento-descricao-painel">
+                        <img src="images/feintec.png"/>
+                        <div class="descricao-evento">
+                            <div class="data"><strong>06 NOV</strong></div>
+                            <div class="titulo"><strong>8ª FEINTEC</strong></div> 
+                            Feira de inovação tecnológica
+                        </div> 
+                </div>
+                
+                <div class="item-evento-descricao-painel">
+                        <img src="images/feintec.png"/>
+                        <div class="descricao-evento">
+                            <div class="data"><strong>06 NOV</strong></div>
+                            <div class="titulo"><strong>8ª FEINTEC</strong></div> 
+                            Feira de inovação tecnológica
+                        </div> 
+                </div>
+                
+                <div class="item-evento-descricao-painel">
+                        <img src="images/feintec.png"/>
+                        <div class="descricao-evento">
+                            <div class="data"><strong>06 NOV</strong></div>
+                            <div class="titulo"><strong>8ª FEINTEC</strong></div> 
+                            Feira de inovação tecnológica
+                        </div> 
+                </div>
+                
+                <div class="item-evento-descricao-painel">
+                        <img src="images/feintec.png"/>
+                        <div class="descricao-evento">
+                            <div class="data"><strong>06 NOV</strong></div>
+                            <div class="titulo"><strong>8ª FEINTEC</strong></div> 
+                            Feira de inovação tecnológica
+                        </div> 
+                </div>
+                
+            </div>
+            
+        </div>
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
